ddec8eca29994577e03ee930a240ebd1bb27829d
Reese-Laptop <Reese60525@gmail.com>
2024-04-02 22:36:43 +0800

    laptop20240402#1

---
+++ Leetcode/205.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+bool isIsomorphic(string s, string t) {
+    map<char, char> char_Correspondence;
+    map<char, bool> is_usedChar;
+    for (int i = 0; i < s.length(); ++i) {
+        if (char_Correspondence[s[i]] == '\0' && !is_usedChar[t[i]]) {
+            char_Correspondence[s[i]] = t[i];
+            is_usedChar[t[i]] = true;
+            continue;
+        }
+        if ((char_Correspondence[s[i]] == '\0' && is_usedChar[t[i]] == true) ||
+            char_Correspondence[s[i]] != t[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    // cin.sync_with_stdio(false); cin.tie(nullptr);
+    string s, t;
+    while (cin >> s >> t) {
+        if (isIsomorphic(s, t))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
