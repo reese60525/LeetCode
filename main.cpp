b91e9dc12c748a0e10244d59ebeaf79a7587f7a4
Reese-Laptop <Reese60525@gmail.com>
2024-05-06 16:05:14 +0800

    2024/05/06

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 static const auto io_sync_off = []() {
@@ -10,47 +11,34 @@ static const auto io_sync_off = []() {
 class Solution {
   public:
     int target = 0;
-    int recursive_solve(int sum, int index, int dp[], std::vector<int> nums) {
+    std::vector<std::vector<int>> dp1;
+    // std::unordered_map<int, std::unordered_map<int, int>> dp;
+    int recursive_solve(int sum, int index, std::vector<int> &nums) {
         // std::cout << "sum:" << sum << " ,index:" << index << ", nums[index]:" << nums[index] << '\n';
-        if (index >= nums.size())
-            return 0;
-        if ((sum + nums[index] > target))
+        if (index >= nums.size() || dp1[index][sum + nums[index]] != 0)
             return -1;
         if (sum + nums[index] == target)
             return 1;
-
-        if (dp[sum + nums[index]] == 0)
-            dp[sum + nums[index]] = recursive_solve(sum + nums[index], index + 1, dp, nums);
-        if (dp[sum] == 0)
-            dp[sum] = recursive_solve(sum, index + 1, dp, nums);
-        if (dp[sum + nums[index]] == 1 || dp[sum] == 1)
+        if ((recursive_solve(sum + nums[index], index + 1, nums) == 1) || recursive_solve(sum, index + 1, nums) == 1)
             return 1;
-        else {
-            // std::cout << "\n*********************************************" << '\n';
-            // std::cout << "sum:" << sum << " ,index:" << index << ", nums[index]:" << nums[index] << '\n';
-            // std::cout << "dp[" << sum + nums[index] << "] = -1" << ", dp[" << sum << "] = -1" << '\n';
-            // std::cout << "*********************************************" << '\n';
-            return -1;
-        }
+        return dp1[index][sum + nums[index]] = -1;
     }
 
     bool canPartition(std::vector<int> &nums) {
-        sort(nums.begin(), nums.end());
         for (auto i : nums)
             target += i;
         if (target & 1)
             return false;
         target /= 2;
-        int dp[target + 1];
-        memset(dp, 0, sizeof(dp));
-        if (recursive_solve(0, 0, dp, nums) == 1)
+        dp1.resize(nums.size(), std::vector<int>(target + 1, -2));
+        if (recursive_solve(0, 0, nums) == 1)
             return true;
         return false;
     }
 };
 int main() {
     Solution solution;
-    std::vector<int> nums {18, 17, 18, 11, 15, 4, 13, 11, 9}; //  (58)
+    std::vector<int> nums {10, 9, 9, 9, 9, 8, 7, 3, 1, 1}; // 1,1,3,9,9,10   7,8,9,9
     std::cout << solution.canPartition(nums) << '\n';
     return 0;
 }
@@ -60,4 +48,5 @@ int main() {
 // [100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,99,97]
 // [14,9,8,4,3,2]
 // [10,9,9,9,9,8,7,3,1,1]
-// [18,17,18,11,15,4,13,11,9]
+// [18,17,18,11,15,4,13,11,9]  => 4,9,11,11,13,15,17,18,18  | 4,9,13,15,17 (58) 41,26,13,4,0
+// [14,75,64,20,95,78,6,98,77,4,71,66,78,44,4,46,77,46,4,84,18,14,52,5,89,26,12,48,71,35,57,10,59,69,35,70,34,80,80,66,91,9,36,91,85,24,46,45,54,86,3,40,30,23,24,4,88,50,42,25,41,60,27,47,92,92,49,50,56,94,9,82,49,41,18,1,23,85,8,48,55,59,82,82,12,84,47,45,41,28,22,15,50,9,64,91,6,94,14,31]
--- main2.cpp
+++ main2.cpp
@@ -1,28 +1,52 @@
 #include <algorithm>
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 
 static const auto io_sync_off = []() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     return nullptr;
 }();
-bool fun(int sum, int index, int target, std::vector<int> nums) {
-    std::cout << "sum:" << sum << " ,index:" << index << ", nums[index]:" << nums[index] << '\n';
-    if (index >= nums.size() || (sum + nums[index] > target))
+class Solution {
+  public:
+    int target = 0;
+    std::unordered_map<int, std::unordered_map<int, int>> dp;
+    int recursive_solve(int sum, int index, std::vector<int> &nums) {
+        // std::cout << "sum:" << sum << " ,index:" << index << ", nums[index]:" << nums[index] << '\n';
+        if (index >= nums.size())
+            return -1;
+        if (sum + nums[index] == target)
+            return 1;
+        if (dp[index][sum + nums[index]] == 0 && recursive_solve(sum + nums[index], index + 1, nums) == 1)
+            return 1;
+        if (dp[index][sum] == 0 && recursive_solve(sum, index + 1, nums) == 1)
+            return 1;
+        return dp[index][sum + nums[index]] = dp[index][sum] = -1;
+    }
+
+    bool canPartition(std::vector<int> &nums) {
+        for (auto i : nums)
+            target += i;
+        if (target & 1)
+            return false;
+        target /= 2;
+        if (recursive_solve(0, 0, nums) == 1)
+            return true;
         return false;
-    if (sum + nums[index] == target) {
-        std::cout << nums[index] << '\n';
-        return true;
     }
-    if (fun(sum + nums[index], index + 1, target, nums) || fun(sum, index + 1, target, nums)) {
-        std::cout << nums[index] << '\n';
-        return true;
-    }
-    return false;
-}
+};
 int main() {
-    std::vector<int> nums {18, 17, 18, 11, 15, 4, 13, 11, 9}; // 4,9,13,15,17 (58)
-    sort(nums.begin(), nums.end());
-    std::cout << "ans:" << fun(0, 0, 58, nums);
+    Solution solution;
+    std::vector<int> nums {18, 17, 18, 11, 15, 4, 13, 11, 9};
+    std::cout << solution.canPartition(nums) << '\n';
     return 0;
 }
+// DATA TEST
+// [1,5,11,5]
+// [1,2,3,5]
+// [100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,99,97]
+// [14,9,8,4,3,2]
+// [10,9,9,9,9,8,7,3,1,1]
+// [18,17,18,11,15,4,13,11,9]  => 4,9,11,11,13,15,17,18,18  | 4,9,13,15,17 (58) 41,26,13,4,0
+// [14,75,64,20,95,78,6,98,77,4,71,66,78,44,4,46,77,46,4,84,18,14,52,5,89,26,12,48,71,35,57,10,59,69,35,70,34,80,80,66,91,9,36,91,85,24,46,45,54,86,3,40,30,23,24,4,88,50,42,25,41,60,27,47,92,92,49,50,56,94,9,82,49,41,18,1,23,85,8,48,55,59,82,82,12,84,47,45,41,28,22,15,50,9,64,91,6,94,14,31]
