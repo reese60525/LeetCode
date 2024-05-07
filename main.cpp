ecab7fe9f9f10dc7b3b1c13f1e5af6376a2dac23
Reese-Laptop <Reese60525@gmail.com>
2024-05-07 12:37:56 +0800

    2024/05/07

--- main.cpp
+++ main.cpp
@@ -1,51 +1,29 @@
-/*
- * 解法 :
- * "32"->17->16->"23"->13->"17"->"8"->3->"6"，""內的值就是題目要找的答案
- * 可以發現如果從最後一個節點往前看，要找的值永遠都大於等於上一個要找的值，因此可以
- * 從最後一個node往前找越來越大的node，最後再從開頭依序更改鏈結(next)就好。
- */
-
 #include <iostream>
 
-static const auto io_sync_off = []() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    return nullptr;
-}();
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
-class Solution {
-  public:
-    ListNode *removeNodes(ListNode *head) {
-        ListNode *last = head;
-        std::vector<ListNode *> ans;
-        int address_diff = head - head->next; // node之間的address差，pre = now + address_diff
-        while (last->next != nullptr) {
-            last = last->next;
+template <int row, int column, int x>
+void test(int (&array_2d)[row][column], int (&array_1d)[x]) {
+    std::cout << "row:" << row << ", column" << column << '\n'; // 顯示array大小
+    std::cout << "x:" << x << '\n';
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < column; ++j) {
+            std::cout << array_2d[i][j] << ' ';
         }
-        int max_value = last->val;
-        while (true) {
-            if (last->val >= max_value) {
-                max_value = last->val;
-                ans.push_back(last);
-            }
-            if (last == head) // 這個要後判斷，否則可能會少一個node。EX:1->1->1->1只有存到1->1->1。
-                break;
-            last += address_diff;
-        }
-        head = ans[ans.size() - 1]; // head為最後一個push進去的node
-        for (int i = ans.size() - 1; i > 0; --i) {
-            ans[i]->next = ans[i - 1];
-        }
-        ans[0]->next = nullptr; // 最後一個node指向nullptr
-        return head;
+        std::cout << '\n';
+    }
+    std::cout << '\n';
+    for (int i = 0; i < x; ++i)
+        std::cout << array_1d[i] << ' ';
+}
+int main() {
+    int array_2d[5][3] = {
+        {1, 2, 3},
+        {1, 2, 3},
+        {1, 2, 3},
+        {1, 2, 3},
+        {1, 2, 3}
     };
-};
-int main() { return 0; }
+    int array_1d[6] = {1, 3, 5, 7, 9, 0};
+    int x = 5;
+    test(array_2d, array_1d);
+    return 0;
+}
