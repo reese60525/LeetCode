fe92fce177c1025ccb2b90966f3178ef0406f90a
Reese-Laptop <Reese60525@gmail.com>
2024-05-08 12:34:19 +0800

    laptop leetcode day5 done

--- main2.cpp
+++ main2.cpp
@@ -1,36 +1,62 @@
+/*
+ * 解法:
+ */
+#include <algorithm>
+#include <functional>
 #include <iostream>
+#include <map>
+#include <vector>
 
-template <int x1, int y1, int x2, int y2>
-void printArray(int (&array1)[x1][y1], int (&array2)[x2][y2]) {
-    std::cout << "array1:" << '\n';
-    for (auto &i : array1) {
-        for (auto &j : i) {
-            std::cout << j << ' ';
+static const auto io_sync_off = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution {
+  public:
+    void radix_sort(std::vector<std::pair<int, int>> &nums) {
+        std::vector<std::pair<int, int>> bucket[1024]; // 1024*1024 > 1e6(測資範圍)
+        // first round
+        for (auto &i : nums) {
+            // 取二進位的後10bits, EX: 10010101 & 00001111 = 00000101
+            bucket[i.first & 1023].emplace_back(i);
         }
-        std::cout << '\n';
-    }
-    std::cout << '\n';
-
-    std::cout << "array2:" << '\n';
-    for (auto &i : array2) {
-        for (auto &j : i) {
-            std::cout << j << ' ';
+        int index = 0;
+        for (auto &i : bucket) {
+            for (auto &j : i) {
+                nums[index++] = j;
+            }
+            i.clear();
         }
-        std::cout << '\n';
+        // second round
+        for (auto &i : nums) {
+            // 取二進位的前10bits, EX: 10010101 >> 4 = 1001
+            bucket[i.first >> 10].emplace_back(i);
+        }
+        index = 0;
+        for (auto &i : bucket) {
+            for (auto &j : i) {
+                nums[index++] = j;
+            }
+        }
     }
-    std::cout << '\n';
-}
+    std::vector<std::string> findRelativeRanks(std::vector<int> &score) {
+        std::vector<std::string> ans(score.size());
+        std::vector<std::pair<int, int>> table;
 
-int main() {
-    int a[2][4] = {
-        {1, 3, 5, 7},
-        {2, 4, 6, 8}
-    };
-    int b[3][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-    printArray(a, b);
-    return 0;
-}
+        for (int i = 0; i < score.size(); ++i)
+            table.emplace_back(score[i], i);
+        radix_sort(table);
+        ans[table[0].second] = "Gold Medal";
+        if (score.size() > 1)
+            ans[table[1].second] = "Silver Medal";
+        if (score.size() > 2)
+            ans[table[2].second] = "Bronze Medal";
+        for (int i = 3; i < score.size(); ++i) {
+            ans[table[i].second] = std::to_string(i + 1);
+        }
+        return ans;
+    }
+};
+int main() { return 0; }
