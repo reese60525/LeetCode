752274231963da9a13eb2b10d660064994f9180f
Reese <reese60525@gamil.com>
2024-10-21 00:03:16 +0800

    update

--- ZeroJudge/a004.cpp
+++ ZeroJudge/a004.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void fun(int x)
-{
+void fun(int x) {
     if (x % 4 == 0 && x % 100 != 0 || x % 400 == 0)
         cout << "閏年" << endl;
     else
         cout << "平年" << endl;
 }
-int main()
-{
+int main() {
     // cin.sync_with_stdio(false); cin.tie(nullptr);
     int a;
     while (cin >> a)
--- tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-
-            if (cur == des) {                  // 當前方向的request都做完了
