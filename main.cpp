d4f364215df27a9d40ad1669073470d2fbbf83fb
Reese <reese60525@gamil.com>
2024-10-22 19:52:49 +0800

    none

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ * 題目:
+ *
+ * 題目解釋:
+ *
+ * 思路:
+ *
+ * 解法:
+ *
+ */
+#include <iostream>
+
+static const auto io_sync_off = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return nullptr;
+}();
+
+int main() {
+
+    return 0;
+}
