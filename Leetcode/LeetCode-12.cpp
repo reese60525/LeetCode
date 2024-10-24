272c8cbae4d81ddbd8d2c10d0c32b574ae4af15e
Reese <reese60525@gamil.com>
2024-10-25 03:19:31 +0800

    leetcode p12 rewrite

--- Leetcode/LeetCode-12.cpp
+++ Leetcode/LeetCode-12.cpp
@@ -1,6 +1,13 @@
+/*
+ * 題目：https://leetcode.com/problems/integer-to-roman/description/
+ *
+ * 題目解釋：
+ * 給一個正整數，轉成羅馬數字。
+ *
+ * 思路：
+ * 建立一組roman_table，從roman_table中最大值開始做轉換。
+ */
 #include <iostream>
-
-using namespace std;
 
 static const auto io_sync_off = []() {
     std::ios::sync_with_stdio(false);
@@ -8,26 +15,43 @@ static const auto io_sync_off = []() {
     return nullptr;
 }();
 
-string intToRoman(int num) {
-    string roman_list[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
-    int arabic_list[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
-    int counter = 12;
-    string ans = "";
-    while (num > 0) {
-        int temp = num / arabic_list[counter];
-        for (int i = 0; i < temp; ++i) {
-            ans += roman_list[counter];
+class Solution {
+  public:
+    std::string intToRoman(int num) {
+        std::pair<int, std::string> roman[] = {
+            {1,    "I" },
+            {4,    "IV"},
+            {5,    "V" },
+            {9,    "IX"},
+            {10,   "X" },
+            {40,   "XL"},
+            {50,   "L" },
+            {90,   "XC"},
+            {100,  "C" },
+            {400,  "CD"},
+            {500,  "D" },
+            {900,  "CM"},
+            {1000, "M" }
+        };
+
+        std::string res = "";
+        int index = 12; // roman的index
+        while (num > 0) {
+            int n = num / roman[index].first; // 羅馬數字要n次
+
+            while (n--) { // res加上n次羅馬數字
+                res += roman[index].second;
+            }
+
+            num %= roman[index].first; // num取餘數
+            --index;                   // 換下一個羅馬數字
         }
-        num %= arabic_list[counter];
-        --counter;
+
+        return res;
     }
-    return ans;
-}
+};
 
 int main() {
-    int num;
-    while (cin >> num) {
-        cout << intToRoman(num) << '\n';
-    }
+
     return 0;
 }
