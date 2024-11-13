c53273e38afd29f74735a9611549248d9604e9f3
Reese <reese60525@gamil.com>
2024-11-13 19:28:45 +0800

    leetcode p19 rewrite

--- Leetcode/LeetCode-19.cpp
+++ Leetcode/LeetCode-19.cpp
@@ -1,6 +1,16 @@
+/*
+ * 題目： https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * 題目解釋：
+ * 給一個link list和正整數n，題目要求移除link list從尾端數來第n個node，以one round來完成。
+ * e.g. link list = [1,2,3,4,5] and n = 2，移除後:link list = [1,2,3,5]。
+ *
+ * 思路：
+ * 以 link list = [1,2,3,4,5,6,7] and n = 2為例，head需要移動四次來到5，並且把5連接到7。
+ * 首先將head移動n=2次後來到3，再將head移動到尾端節點7則需要恰好4步，因此用一個temp代替head，先移動n步，
+ * 再將temp和head一起移動到最後一個node，此時temp在7，head在5，將head->next連接到head->next->next即可。
+ */
 #include <iostream>
-#include <vector>
-using namespace std;
 
 static const auto io_sync_off = []() {
     std::ios::sync_with_stdio(false);
@@ -16,48 +26,30 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-ListNode *removeNthFromEnd(ListNode *head, int n) {
-    ListNode *temp = head;
-    int count = 0;
-    while (temp != nullptr) {
-        ++count;
-        temp = temp->next;
+class Solution {
+  public:
+    ListNode *removeNthFromEnd(ListNode *head, int n) {
+        ListNode *cur = head, *temp = head;
+
+        while (n--) { // temp先移動n步
+            temp = temp->next;
+        }
+
+        if (temp == nullptr) { // 若此時temp為nullptr，代表要刪除的node為head(第一個node)
+            return head->next;
+        }
+
+        while (temp->next != nullptr) { // 同時移動temp和cur，直到temp到最後一個node
+            temp = temp->next;
+            cur = cur->next;
+        }
+        cur->next = cur->next->next;
+
+        return head;
     }
-    // count-(n+1)被刪除的node的前一個node
-    // count-(n-1)被刪除的node的後一個node
-    if (count - (n + 1) < 0) {
-        return head->next;
-    }
-    temp = head;
-    for (int i = 0; i < count - n - 1; ++i) {
-        temp = temp->next;
-    }
-    if (n == 1) {
-        temp->next = nullptr;
-    }
-    else {
-        temp->next = temp->next->next;
-    }
-    return head;
-}
+};
 
 int main() {
-    ListNode *input = new ListNode, *input_head;
-    input_head = input;
-    int num_input[] = {1, 3, 5, 7, 9, 2, 4, 6, 8, 0};
-    // int num_input[] = {1, 2};
-    for (int i = 0; i < size(num_input); ++i) {
-        input->val = num_input[i];
-        if (i < size(num_input) - 1) {
-            input->next = new ListNode;
-            input->next->next = nullptr;
-        }
-        input = input->next;
-    }
-    ListNode *ans = removeNthFromEnd(input_head, 3);
-    while (ans != nullptr) {
-        cout << ans->val << " ";
-        ans = ans->next;
-    }
+
     return 0;
 }
