7d57e8ce4de2a8c7bee8dd3e8b11be4e6caf8218
Reese <reese60525@gamil.com>
2024-11-14 18:29:16 +0800

    leetcode p21 rewrite

--- Leetcode/LeetCode-21.cpp
+++ Leetcode/LeetCode-21.cpp
@@ -5,11 +5,8 @@
  * 給兩個sorted linked list，將兩個list合併成一個sorted linked list。
  *
  * 思路：
- * 對list1創建一個dummy，dummy -> list1，假設list1 = {1, 3, 5}、list2 = {2, 4, 6}、
- * dummy = {0, 1, 3, 5}，要合併成一個sorted linked list，首先判斷list2當前node的value(2)
- * 是否小於dummy->next的value(1)，如果是的話則將其插入，不斷重複這個動作直到dummy->next或
- * list2為nullptr。之後判斷list2的點是否全部插入到list1，如果還有剩餘的node則將其插入到list1
- * 的後面。
+ * 創建一個dummy，依次比較當前list1和list2的值，將較小的node接到dummy後面，直到其中一個list
+ * 做完，之後再將沒做完的list接到dummy後面。
  */
 #include <iostream>
 
@@ -30,39 +27,26 @@ struct ListNode {
 class Solution {
   public:
     ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
-        if (list1 == nullptr) { // list1為空直接return list2
-            return list2;
-        }
+        ListNode *dummy = new ListNode(0, nullptr), *cur = dummy;
 
-        ListNode *dummy = new ListNode(0, list1);
-        list1 = dummy; // list1->next為合併後的linked list head
-
-        while (dummy->next != nullptr && list2 != nullptr) {
-            if (list2->val < dummy->next->val) {
-                // 將兩個list的next存起來
-                ListNode *dummy_next = dummy->next;
-                ListNode *list2_next = list2->next;
-
-                /// 將list2插入到dummy後
-                dummy->next = list2;
-                dummy->next->next = dummy_next;
-
-                // list2往後一個node
-                list2 = list2_next;
+        while (list1 != nullptr && list2 != nullptr) {
+            // 將較小的node接到dummy後面
+            if (list1->val < list2->val) {
+                cur->next = list1;
+                list1 = list1->next;
             }
-            // dummy往後一個node
-            dummy = dummy->next;
-        }
-
-        // 如果list2還有剩餘的node則將其插入到list1後
-        if (list2 != nullptr) {
-            while (dummy->next != nullptr) {
-                dummy = dummy->next;
+            else {
+                cur->next = list2;
+                list2 = list2->next;
             }
-            dummy->next = list2;
+
+            cur = cur->next;
         }
 
-        return list1->next;
+        // 將沒做完的list接到dummy後面
+        cur->next = (list1 != nullptr) ? list1 : list2;
+
+        return dummy->next;
     }
 };
 
