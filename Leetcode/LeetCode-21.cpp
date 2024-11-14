acd945b034de15ba56db4b85c49f1a972a9ada7f
reese's laptop <reese60525@gmail.com>
2024-11-14 10:47:21 +0800

    leetcode p21 rewrite

--- Leetcode/LeetCode-21.cpp
+++ Leetcode/LeetCode-21.cpp
@@ -1,91 +1,72 @@
+/*
+ * 題目： https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * 題目解釋：
+ * 給兩個sorted linked list，將兩個list合併成一個sorted linked list。
+ *
+ * 思路：
+ * 對list1創建一個dummy，dummy -> list1，假設list1 = {1, 3, 5}、list2 = {2, 4, 6}、
+ * dummy = {0, 1, 3, 5}，要合併成一個sorted linked list，首先判斷list2當前node的value(2)
+ * 是否小於dummy->next的value(1)，如果是的話則將其插入，不斷重複這個動作直到dummy->next或
+ * list2為nullptr。之後判斷list2的點是否全部插入到list1，如果還有剩餘的node則將其插入到list1
+ * 的後面。
+ */
 #include <iostream>
-using namespace std;
 
-struct ListNode
-{
+static const auto io_sync_off = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return nullptr;
+}();
+
+struct ListNode {
     int val;
     ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
-{
-    ListNode *ans = new ListNode, *ans_head = ans;
-    if (list1 == NULL) // 空的linked list回傳另一個linked list
-        return list2;
-    else if (list2 == NULL)
-        return list1;
-    while (true)
-    {
-        if (list1->val > list2->val)
-        {
-            ans->next = list2;
-            list2 = list2->next;
+class Solution {
+  public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+        if (list1 == nullptr) { // list1為空直接return list2
+            return list2;
         }
-        else
-        {
-            ans->next = list1;
-            list1 = list1->next;
-        }
-        if (list1 == NULL)
-        {
-            ans->next->next = list2;
-            break;
-        }
-        else if (list2 == NULL)
-        {
-            ans->next->next = list1;
-            break;
-        }
-        else
-            ans = ans->next;
-    }
-    return ans_head->next; // ans_head為空，從第二個開始
-}
 
-int main()
-{
-    // cin.sync_with_stdio(false); cin.tie(nullptr);
-    ListNode *list1 = new ListNode, *list2 = new ListNode, *list1_head = list1, *list2_head = list2;
-    cout << "\r\nlist1_address: " << list1_head << ", list1_next: " << list1->next << ", list1_val: " << list1_head->val << endl;
-    list1->next = NULL;
-    list2->next = NULL;
-    int x = 3, y = 3;
-    for (int i = 0; i < x; ++i)
-    {
-        if (i < x - 1)
-        {
-            list1->next = new ListNode;
-            list1->next->next = NULL;
+        ListNode *dummy = new ListNode(0, list1);
+        list1 = dummy; // list1->next為合併後的linked list head
+
+        while (dummy->next != nullptr && list2 != nullptr) {
+            if (list2->val < dummy->next->val) {
+                // 將兩個list的next存起來
+                ListNode *dummy_next = dummy->next;
+                ListNode *list2_next = list2->next;
+
+                /// 將list2插入到dummy後
+                dummy->next = list2;
+                dummy->next->next = dummy_next;
+
+                // list2往後一個node
+                list2 = list2_next;
+            }
+            // dummy往後一個node
+            dummy = dummy->next;
         }
-        cin >> list1->val;
-        list1 = list1->next;
-    }
-    // while (list1_head != NULL)
-    // {
-    //     cout << "ans_address: " << list1_head << ", ans_nest" << list1->next << ", ans_val: " << list1_head->val << endl;
-    //     list1_head = list1_head->next;
-    // }
-    for (int i = 0; i < y; ++i)
-    {
-        if (i < y - 1)
-        {
-            list2->next = new ListNode;
-            list2->next->next = NULL;
+
+        // 如果list2還有剩餘的node則將其插入到list1後
+        if (list2 != nullptr) {
+            while (dummy->next != nullptr) {
+                dummy = dummy->next;
+            }
+            dummy->next = list2;
         }
-        cin >> list2->val;
-        list2 = list2->next;
+
+        return list1->next;
     }
-    // while (list2_head != NULL)
-    // {
-    //     cout << "ans_address: " << list2_head << ", ans_val: " << list2_head->val << endl;
-    //     list2_head = list2_head->next;
-    // }
-    ListNode *ans;
-    ans = mergeTwoLists(list1_head, list2_head);
-    while (ans != NULL)
-    {
-        cout << "ans_address: " << ans << ", ans_val: " << ans->val << endl;
-        ans = ans->next;
-    }
+};
+
+int main() {
+
     return 0;
 }
