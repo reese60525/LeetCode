f82bfb7309456b9798033cce1f9de26f0d626359
reese's laptop <reese60525@gmail.com>
2024-11-14 11:27:06 +0800

    leetcode p22 rewrite

--- Leetcode/LeetCode-22.cpp
+++ Leetcode/LeetCode-22.cpp
@@ -1,7 +1,16 @@
+/*
+ * 題目： https://leetcode.com/problems/generate-parentheses/description/
+ *
+ * 題目解釋：
+ * 給一個數字n，找出所有strings，這些string由n組左右括號排列組成一個valid括號string。
+ * e.g. n = 1，strings = {"()"}。 n = 2，strings = {"()()", "(())"}。
+ *
+ * 思路：
+ * 用backtracking algorithm，每次只添加一個 '(' 或 ')' 到string，並且用兩個整數l和r
+ * 來記錄當前左右括號的數量，當(l < r || l > n || r > n)時表示當前string not valid，
+ * 若(l == n && r === n)表示當前string valid，將其加入res。
+ */
 #include <iostream>
-#include <vector>
-
-using namespace std;
 
 static const auto io_sync_off = []() {
     std::ios::sync_with_stdio(false);
@@ -9,29 +18,41 @@ static const auto io_sync_off = []() {
     return nullptr;
 }();
 
-void recursive_ans(vector<string> &ans, int n, int left_count, int right_count, string temp) {
-    // cout << "left_count= " << left_count << ", right_count= " << right_count << ", temp= " <<
-    // temp
-    //  << '\n';
+class Solution {
+  public:
+    std::vector<std::string> generateParenthesis(int n) {
+        int l = 0, r = 0;
+        std::string s;
+        std::vector<std::string> res;
 
-    if (left_count == n && right_count == n) {
-        ans.push_back(temp);
+        backTracking(res, s, n, 0, 0);
+
+        return res;
     }
-    if (left_count < n)
-        recursive_ans(ans, n, left_count + 1, right_count, temp + '(');
-    if (right_count < left_count)
-        recursive_ans(ans, n, left_count, right_count + 1, temp + ')');
-}
-vector<string> generateParenthesis(int n) {
-    vector<string> ans;
-    recursive_ans(ans, n, 0, 0, "");
-    return ans;
-}
+
+  private:
+    void backTracking(std::vector<std::string> &res, std::string &s, int &n, int l, int r) {
+        if (l < r || l > n || r > n) { // not valid
+            return;
+        }
+
+        if (l == n && r == n) { // valid string
+            res.push_back(s);
+            return;
+        }
+
+        // 進入下一層backtracking
+        s += '(';
+        backTracking(res, s, n, l + 1, r);
+        s.pop_back();
+
+        s += ')';
+        backTracking(res, s, n, l, r + 1);
+        s.pop_back();
+    }
+};
 
 int main() {
-    vector<string> ans = generateParenthesis(3);
-    for (auto i : ans) {
-        cout << i << " ";
-    }
+
     return 0;
 }
