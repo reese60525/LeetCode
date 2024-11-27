e3d301b2e6cb795cc92bb651f61d43814c582389
reese's laptop <reese60525@gmail.com>
2024-11-27 16:28:05 +0800

    leetcode p3243 修改

--- Leetcode/LeetCode-3243.cpp
+++ Leetcode/LeetCode-3243.cpp
@@ -7,10 +7,11 @@
  * 求每加入一個新的query後，從起點到終點(C0到Cn)的最短路徑。
  *
  * 思路：
- * 用bfs來解決最短路徑問題。先創建一個array shortest_distance用來儲存從C0到Ci的最短距離，
- * 每當有新的query(Cx, Cy)加入時，只會影響到從Cy開始連通的所有城市的最短距離，因此bfs只要
- * 從Cy開始，並且最短距離有變小的城市將其最短距離更新，然後繼續bfs，最後Cn的最短距離即為加入
+ * 可以用dfs或bfs來解決最短路徑問題。先創建一個array shortest_distance用來儲存從C0到Ci的最短距離，
+ * 每當有新的query(Cx, Cy)加入時，只會影響到從Cy開始連通的所有城市的最短距離，因此只要從Cy開始，並且
+ * 將最短距離有變小的城市的最短距離更新，然後對後續相連接的城市做同樣的事情，最後Cn的最短距離即為加入
  * 該query後的最短距離。
+ * 該題可以用dfs或bfs來解決，dfs的時間、空間表現更好，估計是因為bfs要維護queue。
  */
 #include <iostream>
 #include <vector>
@@ -23,17 +24,47 @@ static const auto io_sync_off = []() {
 
 class Solution {
   public:
+    // dfs解法
     void findShortestDistance(std::vector<std::vector<int>> &paths, std::vector<int> &shortest_distance,
                               int curr_city) {
         // 遍歷Cy連通的所有city
         for (int next_city : paths[curr_city]) {
-            // 只有當連通的city其最短距離能變更小才需要更新其值並繼續bfs
+            // 只有當連通的city其最短距離能變更小才需要更新其值並繼續遞迴
             if (shortest_distance[next_city] > shortest_distance[curr_city] + 1) {
                 shortest_distance[next_city] = shortest_distance[curr_city] + 1;
                 findShortestDistance(paths, shortest_distance, next_city);
             }
         }
     }
+
+    // // bfs解法
+    // void findShortestDistance(std::vector<std::vector<int>> &paths, std::vector<int> &shortest_distance,
+    //                           int curr_city) {
+    //     int end = paths.size() - 1; // 終點
+    //     std::queue<int> q;
+    //     q.push(curr_city);
+
+    //     while (!q.empty()) {
+    //         int curr_city = q.front();
+    //         q.pop();
+
+    //         // 到達終點就可以提前離開bfs
+    //         if (curr_city == end) {
+    //             return;
+    //         }
+
+    //         // 遍歷Cy連通的所有city
+    //         for (int next_city : paths[curr_city]) {
+    //             // 最短路徑有變小才需要更新其值並放入queue中bfs
+    //             if (shortest_distance[curr_city] + 1 >= shortest_distance[next_city]) {
+    //                 continue;
+    //             }
+    //             shortest_distance[next_city] = shortest_distance[curr_city] + 1;
+
+    //             q.push(next_city);
+    //         }
+    //     }
+    // }
 
     std::vector<int> shortestDistanceAfterQueries(int n, std::vector<std::vector<int>> &queries) {
         // paths[i]儲存city_i可以直通哪些city
@@ -55,7 +86,7 @@ class Solution {
             // 更新query(Cx, Cy)的Cy最短距離
             shortest_distance[queries[i][1]] =
                 std::min(shortest_distance[queries[i][1]], shortest_distance[queries[i][0]] + 1);
-            // bfs更新Cy連通的所有城市最短距離
+            // 更新從Cy開始所有連通的城市的最短距離
             findShortestDistance(paths, shortest_distance, queries[i][1]);
             res[i] = shortest_distance[n - 1];
         }
