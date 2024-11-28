5a802d8a43f3dccae6c0116c5f8a183e60a14d8d
Reese <reese60525@gamil.com>
2024-11-28 21:37:51 +0800

    leetcode p24 rewrite

--- Leetcode/LeetCode-20.cpp
+++ Leetcode/LeetCode-20.cpp
@@ -20,6 +20,20 @@ static const auto io_sync_off = []() {
     return nullptr;
 }();
 
+/*
+ * 題目解釋：
+ * 給一個由「大中小括號」組成字串，判斷該字串是否合法。
+ * [] is valid, ][] is not valid, ][ is not valid.
+ *
+ * 思路：
+ * 對於該string，從頭開始遍歷每個字元，當遇到第一個右括號時，其左方的字元必須是相對應的
+ * 左括號，e.g.
+ * 對於"(({["接下來的遇到的右括號得是']'，因此可以建立一個stack，當遇到左
+ * 括號時就將其放入stack，若是遇到右括號就判斷stack的top是否為相對應的左括號，是的話就將
+ * stack的top
+ * pop掉，整個string遍歷後stack應該為空(每個左括號都有對應到一個右括號)。
+ */
+
 class Solution {
   public:
     bool isValid(std::string s) {
@@ -30,16 +44,13 @@ class Solution {
                 st.push(c);
             }
             else {
-                if (st.empty()) { // 若stack為空時遇到右括號，e.g. string = ")"，則return false
+                // 若stack為空，或是括號不能對應，則return false
+                if (st.empty() || (c == ')' && st.top() != '(') || (c == ']' && st.top() != '[') ||
+                    (c == '}' && st.top() != '{')) {
                     return false;
                 }
-                // 若stack不為空，則判斷stack的top是否為相對應的左括號，是的話就將stack的top pop掉
-                if (c == ')' && st.top() == '(' || c == ']' && st.top() == '[' || c == '}' && st.top() == '{') {
-                    st.pop();
-                }
-                else {
-                    return false;
-                }
+
+                st.pop();
             }
         }
 
--- Leetcode/LeetCode-24.cpp
+++ Leetcode/LeetCode-24.cpp
@@ -1,58 +1,46 @@
+/*
+ * 題目： https://leetcode.com/problems/swap-nodes-in-pairs/description/
+ *
+ * 題目解釋：
+ * 給一個整數linked list，從head node開始，將相鄰的node的值交換。
+ * e.g. {} -> {}， {1} -> {1}， {1, 2} -> {2, 1}， {1, 2, 3} -> {2, 1, 3}
+ *
+ * 思路：
+ * 要交換的兩個node不能是nullptr才能交換。
+ */
 #include <iostream>
 
 static const auto io_sync_off = []() {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-  return nullptr;
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return nullptr;
 }();
 
 struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 class Solution {
-public:
-  ListNode *swapPairs(ListNode *head) {
-    // link_list 為空或只有一個node
-    if (head == nullptr || head->next == nullptr)
-      return head;
+  public:
+    ListNode *swapPairs(ListNode *head) {
+        ListNode *cur = head;
 
-    ListNode *pre = new ListNode, *now = new ListNode;
-    now = head;
-    // return的head為第二個node
-    head = head->next;
-    // 千萬要小心別使用到空指標
-    while (now != nullptr && now->next != nullptr) {
-      pre->next = now->next;
-      now->next = now->next->next;
-      pre->next->next =
-          now; // 如果是now->next->next是錯誤的，因為now->next的值已經在上一行改了!
-      pre = now;
-      now = now->next;
+        while (cur != nullptr && cur->next != nullptr) {
+            int temp = cur->val;
+            cur->val = cur->next->val;
+            cur->next->val = temp;
+            cur = cur->next->next;
+        }
+
+        return head;
     }
-    return head;
-  }
 };
 
 int main() {
-  Solution solution;
-  ListNode *head = new ListNode, *new_head = new ListNode;
-  new_head = head;
-  int nums[]{1, 2, 3, 4};
-  for (int i = 0; i < 4; ++i) {
-    head->val = nums[i];
-    if (i == 3) {
-      head->next = nullptr;
-      break;
-    }
-    ListNode *new_node = new ListNode;
-    head->next = new_node;
-    head = head->next;
-  }
-  solution.swapPairs(new_head);
-  return 0;
+
+    return 0;
 }
