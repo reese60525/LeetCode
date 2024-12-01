43f60a57ed92fd376f1161430a28c61b835952b3
reese's laptop <reese60525@gmail.com>
2024-12-01 20:46:31 +0800

    leetcode p28 rewrite

--- Leetcode/LeetCode-28.cpp
+++ Leetcode/LeetCode-28.cpp
@@ -1,4 +1,19 @@
+/*
+ * 題目： https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+ *
+ * 題目解釋：
+ * 給一個 string haystack 和 needle，如果 haystack 中有 substring = needle，回傳起始 index
+ * 最小的 substring 之起始 index，如果沒有則 return -1。
+ * e.g.
+ * haystack = "sadbutsad"，needle = "sad"
+ * substring 有兩個和 "sad" 相同，回傳起始 index 比較小的，也就是 0。
+ *
+ * 思路：
+ * string 匹配問題可以用 KMP algorithm。
+ * KMP algorithm的介紹：https://yeefun.github.io/kmp-algorithm-for-beginners/
+ */
 #include <iostream>
+#include <string>
 
 static const auto io_sync_off = []() {
     std::ios::sync_with_stdio(false);
@@ -6,85 +21,56 @@ static const auto io_sync_off = []() {
     return nullptr;
 }();
 
-// KMP Algorithm
 class Solution {
   public:
-    // n = needle.length(), 建表時間複雜度O(n^2);
-    // abcabe
-    // i = 0, sub = a
-    // i = 1, sub = ab, (a,b)
-    // i = 2, sub = abc, (ab,bc)、(a,c)
-    // i = 3, sub = abca, (abc,bca)、(ab,ca)、(a,a) => next[3] = 1;
-    // i = 4, sub = abcab, (abca,bcab)、(abc,cab)、(ab,ab) => next[4] = 2;
-    // int *get_next(std::string needle) {
-    //     // needle本身不需要建表，因為當haystack有substring和needle一樣就會直接return index了。
-    //     int *next = new int[needle.length() - 1];
-    //     memset(next, 0, (needle.length() - 1) * sizeof(*next)); // size記得-1
-    //     for (int i = 0; i < needle.length() - 1; ++i) {
-    //         std::string sub = needle.substr(0, i + 1);
-    //         for (int j = sub.length() - 1; j >= 1; --j) {
-    //             if (sub.substr(0, j) == sub.substr(sub.length() - j, j)) {
-    //                 next[i] = j;
-    //                 break; // 找到最長的就break，以免後續短的取代掉長的。
-    //             }
-    //         }
-    //     }
-    //     return next;
-    // }
+    int strStr(std::string haystack, std::string needle) {
+        // 如果 needle 比 haystack 還長，不可能匹配，return -1
+        if (haystack.size() < needle.size()) {
+            return -1;
+        }
 
-    // m = needle.length(), 建表時間複雜度O(n);
-    int *getNext(std::string needle) {
-        // next[0~needle-1] : 0個重覆到needle.length()-1個重複字元
-        int j = 0;
-        int *next = new int[needle.length() + 1];
-        memset(next, 0, (needle.length() + 1) * sizeof(*next));
-        for (int i = 1; i < needle.length(); ++i) {
+        // 建構 LPS(Longest Prefix and Suffix) table
+        std::vector<int> LPS_table(needle.size(), 0);
+        int j = 0; // LPS 的 index
+        // i 從 1 開始，因為 needle 長度為 1 的 prefix 的 LPS 長度必為 0
+        for (int i = 1; i < needle.size(); ++i) {
+            // 如果不匹配，回退到上一個可能的 prefix 位置，從該位置繼續比對
+            while (j > 0 && needle[i] != needle[j]) {
+                j = LPS_table[j - 1];
+            }
+            // 如果匹配，增加 prefix 長度
             if (needle[i] == needle[j]) {
-                if (next[i] > 0)
-                    next[i + 1] = next[i] + 1;
-                else
-                    next[i + 1] = 1;
                 ++j;
             }
-            else if (next[i] > 0) {
-                int back = j - next[j];
-                if (needle[j - back] == needle[i])
-                    next[i + 1] = next[j] + 1;
-                else {
-                    next[i + 1] = next[j];
-                    j = 0;
-                }
+
+            LPS_table[i] = j;
+        }
+
+        // 開始比對 haystack 和 needle
+        j = 0; // needle 的 index
+        for (int i = 0; i < haystack.size(); ++i) {
+            // 如果不匹配，回退到上一個可能的 prefix 位置，從該位置繼續比對
+            while (j > 0 && haystack[i] != needle[j]) {
+                j = LPS_table[j - 1];
+            }
+
+            // 如果匹配，移動 needle 的 index
+            if (haystack[i] == needle[j]) {
+                ++j;
+            }
+
+            // 如果已經匹配整個 needle，return 起始 Iindex
+            if (j == needle.size()) {
+                return i - j + 1;
             }
         }
-        return next;
-    }
-    int strStr(std::string haystack, std::string needle) {
-        int *next = getNext(needle); // next[i]代表string有i+1個字相同
-        // for (int i = 0; i <= needle.length(); ++i)
-        //     std::cout << next[i] << ' ';
-        // std::cout << "\n";
-        int len = haystack.length() - needle.length(), round = needle.length();
-        for (int i = 0; i <= len;) {
-            for (int j = 0; j < round; ++j) {
-                if (haystack[i + j] != needle[j]) {
-                    if (j == 0)
-                        ++i;
-                    else
-                        i += (j - next[j]);
-                    break;
-                }
-                if (j == round - 1)
-                    return i;
-            }
-        }
+
+        // 沒有找到，return -1
         return -1;
     }
 };
 
 int main() {
-    Solution solution;
-    std::string a, b;
-    while (std::cin >> a >> b)
-        std::cout << solution.strStr(a, b) << '\n';
+
     return 0;
 }
