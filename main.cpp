f2c76a1f5464d614f5017f2e7c40d852d9d7fd2d
Reese <reese60525@gamil.com>
2024-12-19 20:09:11 +0800

    leetcode p768 done

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
-/******************************************
- *  File        : main.cpp
+/*
  *  Author      : Reese
- *  Created On  : 2024-12-19 11:56
- *  Description : https://leetcode.com/problems/max-chunks-to-make-sorted-ii/description/
- ******************************************/
+ *  Created On  : 2024-12-19 17:59
+ *  Description :
+ */
 
 #include <iostream>
-#include <stack>
-#include <vector>
 
 static const auto io_sync_off = []() {
     std::ios::sync_with_stdio(false);
@@ -15,43 +12,7 @@ static const auto io_sync_off = []() {
     return nullptr;
 }();
 
-class Solution {
-  public:
-    int maxChunksToSorted(std::vector<int> &arr) {
-        std::stack<int> st;
-
-        for (int n : arr) {
-            if (!st.empty() && st.top() > n) {
-                int max_val = st.top();
-                while (!st.empty() && st.top() > n) {
-                    st.pop();
-                }
-                st.push(max_val);
-            }
-            else {
-                st.push(n);
-            }
-        }
-
-        return st.size();
-    }
-};
-
-// 1 2 3 1 1 2 3 4 3
-// 1 > 1
-// 1 2 > 1 2
-// 1 2 3 > 1 2 3
-// 1 2 3 1 > 1 3
-// 1 2 3 1 1 > 1 3
-// 1 2 3 1 1 2 > 1 3
-// 1 2 3 1 1 2 3 > 1 3 3
-// 1 2 3 1 1 2 3 4 > 1 3 3 4
-// 1 2 3 1 1 2 3 4 3 > 1 3 3 4
-// 1 2 3 1 1 2 3 4 1 > 1 4
-
 int main() {
-    Solution s;
-    std::vector<int> arr = {};
-    s.maxChunksToSorted(arr);
+
     return 0;
 }
